a47e7e0c05b40bb7c65aea3662060a93a9e77327
Anwesh43 <anweshthecool0@gmail.com>
2019-05-15 09:34:29 +0530

    adding drawLSSNode canvas extension method

--- lineseparatorstepview/src/main/java/com/anwesh/uiprojects/lineseparatorstepview/LineSeparatorStepView.kt
+++ lineseparatorstepview/src/main/java/com/anwesh/uiprojects/lineseparatorstepview/LineSeparatorStepView.kt
@@ -31,3 +31,33 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawLSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    var x : Float = 0f
+    val lineGap : Float = (2 * size) / (lines - 1)
+    var y : Float = size
+    x = lineGap * sc1.divideScale(0, lines)
+    drawLine(x, size, x, size - lineGap, paint)
+    for (j in 1..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        x -= (2 * lineGap * (1f - 2 * (j % 2)))
+        y -= lineGap * sc
+        save()
+        translate(x, y)
+        drawLine(0f, 0f, 0f, -lineGap, paint)
+        restore()
+    }
+    restore()
+}
