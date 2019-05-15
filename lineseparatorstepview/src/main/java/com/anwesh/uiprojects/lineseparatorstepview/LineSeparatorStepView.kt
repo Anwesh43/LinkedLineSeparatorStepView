90891aa433e2f19aecb667396baaf4666001052f
Anwesh43 <anweshthecool0@gmail.com>
2019-05-15 09:55:11 +0530

    adding LSSNode class

--- lineseparatorstepview/src/main/java/com/anwesh/uiprojects/lineseparatorstepview/LineSeparatorStepView.kt
+++ lineseparatorstepview/src/main/java/com/anwesh/uiprojects/lineseparatorstepview/LineSeparatorStepView.kt
@@ -126,4 +126,48 @@ class LineSeparatorStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSSNode(var i : Int, val state : State = State()) {
+
+        private var next : LSSNode? = null
+        private var prev : LSSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LSSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSSNode {
+            var curr : LSSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
